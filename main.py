70b2a2050f6c34470f910b288d74182163b92fd7
sohan <sohanreddy28@gmail.com>
2025-12-28 14:05:26 +0530

    feat: Upgrade main loop logic to include ChitManager      User can manage multiple chitlists at once

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from src.blueprint import *
 
-chitlist_1 = Chit(1)
-
-def add_members():
+chit_manager = ChitManager()
+def add_members(chitlist):
     n = int(input("Enter the number of members: "))
     for i in range(n):
-        chitlist_1.add_member()
-def main():
-    choice = 0
+        chitlist.add_member()
+
+def chitlist_operations(chitlist):
     while True:
         print("---")
         print("1. Add Members\n2. Show Members\n3. Ongoing month\n4. Lottery\n5. Set month\n6. View creation time\n7. View outstading fees\n444.Exit")
@@ -16,23 +15,44 @@ def main():
 
         match(choice):
             case '1':
-                add_members()
+                add_members(chitlist)
             case '2':
-                chitlist_1.display_members()
+                chitlist.display_members()
             case '3':
-                print("Current month is ", chitlist_1.month)
+                print("Current month is ", chitlist.month)
             case '4':
-                chitlist_1.lottery()
+                chitlist.lottery()
             case '5':
-                chitlist_1.set_month()
+                chitlist.set_month()
             case '6':
-                chitlist_1.display_creation_time()
+                chitlist.display_creation_time()
             case '7':
-                chitlist_1.current_month_pay()
+                chitlist.current_month_pay()
             case '444':
-                break
+                return
             case _:
                 print("Enter a valid number")
+
+
+def main():
+    choice = 0
+    while True:
+        print("---")
+        print("1. Create ChitList\n2. Display Chitlists\n3. Chitlist Ops.\n444.Exit")
+        print("---")
+        choice = input()
+        match(choice):
+            case '1':
+                chit_manager.create_chitlist()
+            case '2':
+                chit_manager.display_chitlists()
+            case '3':
+                chit_manager.display_chitlists()
+                i = int(input("Enter an id (left index is id): "))
+                chitlist_operations(chit_manager.chitlists[i-1])
+            case '444':
+                break
+
 
 if __name__ == "__main__":
     main()
