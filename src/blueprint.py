a1ef455aa4215696e030e1eaa92d6bf7baeb555a
sohan <sohanreddy28@gmail.com>
2025-12-28 11:29:42 +0530

    fix: off by one error in set_month

--- src/blueprint.py
+++ src/blueprint.py
@@ -7,28 +7,27 @@ class ChitManager:
     def tally(chit_amount):
         if(chit_amount == 1 or chit_amount == 2):
             return 20 #Standard for my use case
-        elif(chit_amount == 5):
+        elif(chit_amount == 5 or chit_amount == 2.5):
             return 25
         else:
             return 0
 class Chit:
     total_chit_lists = 0
-    def remove(self,id):
-        self.not_winners[id] = self.not_winners[-1]
-        if not len(self.not_winners) == 0:
-            self.not_winners.pop()  
-        return
     def __init__(self,chit_amount):
         self.chit_amount = chit_amount
         self.chit_list = dict()
         self.creation_time = str(datetime.now())
         self.names_set = set() #Used for quickly checking if a name already exists.
         self.counter = 1 #Counts Members objects, sequentially store members
-        #self.winners = [i for i in range(1,ChitManager.tally(chit_amount) + 1)] dropped
-        self.winners_map = dict()
-        self.not_winners = []
+        self.winners_map = dict() #maps month (key) to winner (member object)
+        self.not_winners = [] #Holds member objects who haven't won yet. Note: total months - current month = len(not_winners) is a necessary condition
         self.month = 1
         Chit.total_chit_lists += 1
+    def remove(self,id):
+        self.not_winners[id] = self.not_winners[-1]
+        if not len(self.not_winners) == 0:
+            self.not_winners.pop()  
+        return
     def add_member(self):
         while True:
             name = input("Enter name: ")
@@ -47,6 +46,14 @@ class Chit:
         print("ChitList ", self.creation_time)
         for i in range(1,len(self.chit_list) + 1):
             print(i, " ", self.chit_list.get(i).name)
+    def current_month_pay(self):
+        self.display_members()
+        for i in self.chit_list:
+            if i in self.not_winners:
+                current_month_pay = (self.chit_amount * 100000) / ChitManager.tally(self.chit_amount)
+            else:
+                current_month_pay = ((self.chit_amount * 100000) / ChitManager.tally(self.chit_amount)) * 1.01
+            print(i.id, " ", i.name, ": ", current_month_pay)
     def display_creation_time(self):
         print(self.creation_time)
     def display_note(self, id):
@@ -65,10 +72,8 @@ class Chit:
     def lottery(self):
         winner = choice(self.not_winners)
         print("Winner is \nId: ", winner.id, "Name: ", winner.name)
-        #print("List ", self.not_winners, "id ", winner.id)
         i = self.not_winners.index(winner)
         self.remove(i)
-        #print("List ", self.not_winners, "id ", winner.id)
         self.winners_map[self.month] = winner
         self.month += 1  
     def set_month(self):
@@ -77,7 +82,8 @@ class Chit:
         print("Select a member")
         id = int(input("Enter id: "))
         winner = self.chit_list[id]
-        self.remove(id-1)
+        i = self.not_winners.index(winner)
+        self.remove(i)
         print("Set ",winner.name, " as winner")
         self.winners_map[self.month] = winner
         self.month += 1
@@ -88,8 +94,6 @@ class Member:
         self.note = note
         self.id = -1
         self.creation_time = str(datetime.now())
-
-
 
 
 if __name__ == "__main__": #Temporary: Testing/Debugging 
